4c42f14e2800ca45e0b83e4b6d43c8b2026d9066
Raghul <raghul1826@gmail.com>
2025-08-30 20:06:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -439,4 +439,4 @@ def upload_to_drive():
 # ------------------ App Initialization ------------------
 if __name__ == "__main__":
     # Use debug=True for development for better error messages
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
