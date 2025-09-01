a1eb955fc9bcb3d836f0ef72d1d0d73ca068bff3
Raghul <raghul1826@gmail.com>
2025-09-01 07:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -648,4 +648,5 @@ if __name__ == "__main__":
     with app.app_context():
         init_db()
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
